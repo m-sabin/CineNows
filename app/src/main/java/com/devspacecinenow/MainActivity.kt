f484bfccc5f0aec8d164ffbba3332ae4204bae51
Missagne <scriptnibas@gmail.com>
2025-06-17 17:47:41 -0300

    consumo das imagens do NowPlaying via o API

--- app/src/main/java/com/devspacecinenow/MainActivity.kt
+++ app/src/main/java/com/devspacecinenow/MainActivity.kt
@@ -1,24 +1,14 @@
 package com.devspacecinenow
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.devspacecinenow.ui.theme.CineNowTheme
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/devspacecinenow/MovieListScreen.kt
+++ app/src/main/java/com/devspacecinenow/MovieListScreen.kt
@@ -2,10 +2,13 @@ package com.devspacecinenow
 
 
 import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -13,7 +16,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 
 @Composable
@@ -24,18 +29,56 @@ fun MovieListScreen() {
         try {
             val apiService = RetrofitClient.retrofitInstance.create(ApiService::class.java)
             val response = apiService.getNowPlayingMovies()
-            Log.d("MovieListScreen", "Resposta recebida: $response")
+            Log.d("MovieListScreen", "Response receive: $response")
             nowPlayingMovies = response.results.orEmpty()
-            Log.d("MovieListScreen", "Lista de filmes: $nowPlayingMovies")
+            Log.d("MovieListScreen", "Movies List: $nowPlayingMovies")
 
         } catch (e: Exception) {
-            Log.e("MovieListScreen", "Erro na requisição", e)
+            Log.e("MovieListScreen", "Request error", e)
         }
     }
-    LazyColumn {
-        items(nowPlayingMovies) { movie ->
-            Text(text = movie.title)
+
+    MovieList(
+        nowPlayingMovies
+
+    ) { movieClicked -> }
+
+}
+
+@Composable
+fun MovieList(
+    movieList: List<MovieDto>,
+    onClick: (MovieDto) -> Unit
+) {
+    LazyRow {
+        items(movieList) {
+            MovieItem (
+                movieDto = it,
+                onClick = onClick
+            )
         }
+    }
+}
+
+@Composable
+fun MovieItem(
+    movieDto: MovieDto,
+    onClick: (MovieDto) -> Unit
+) {
+    Column (
+        modifier = Modifier.clickable{
+            onClick.invoke(movieDto)
+        }
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .width(120.dp)
+                .height(150.dp)
+                .padding(end = 4.dp),
+            model = movieDto.posterFullPath,
+            contentScale = ContentScale.Crop,
+            contentDescription = "${movieDto.title} Poster Path"
+        )
     }
 
 }
